80753e755717d61a4beb872d539ddf53746a800e
Marzio95 <marziodellarocca14@gmail.com>
2022-03-02 18:06:42 +0100

    pulisco  il codice

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
 
 const containerGame = document.querySelector('.container_game');
 
-// for (let i = 1; i <= 49; i++) {
-
-//    let box = document.createElement('div');
-//    console.log(box);
-//    box.classList.add('quadrato');
-//    box.innerHTML = i;
-//    containerGame.append(box);   
-// }
-
 const btnPlay = document.getElementById('button_play');
 let difficultInput = document.getElementById('difficult');
-
-
 
 btnPlay.addEventListener('click', myGame);
 
