8487d645d9aecd12ca4733b50b971faff1b00ea9
Marzio95 <marziodellarocca14@gmail.com>
2022-03-02 16:56:00 +0100

    formatto js

--- js/script.js
+++ js/script.js
@@ -15,31 +15,32 @@ let difficultInput = document.getElementById('difficult');
 
 btnPlay.addEventListener('click', myGame);
 
-function myGame (){
-    if(difficultInput.value == 'Easy') {
-        
-    for (let i = 1; i <= 49; i++) {
-    let box = document.createElement('div');
-    box.classList.add('quadrato');
-    box.innerHTML = i;
-    containerGame.append(box);   
+function myGame() {
+    if (difficultInput.value == 'Easy') {
+
+        for (let i = 1; i <= 49; i++) {
+            let box = document.createElement('div');
+            box.classList.add('quadrato');
+            box.innerHTML = i;
+            containerGame.append(box);
+        }
+    } else if (difficultInput.value == 'Medium') {
+
+        for (let i = 1; i <= 81; i++) {
+            let box = document.createElement('div');
+            box.classList.add('quadrato2');
+            box.innerHTML = i;
+            containerGame.append(box);
+        }
+    } else {
+
+        for (let i = 1; i <= 100; i++) {
+            let box = document.createElement('div');
+            box.classList.add('quadrato3');
+            box.innerHTML = i;
+            containerGame.append(box);
+        }
     }
-  } else if (difficultInput.value == 'Medium'){
-      
-    for (let i = 1; i <= 81; i++) {
-        let box = document.createElement('div');
-        box.classList.add('quadrato2');
-        box.innerHTML = i;
-        containerGame.append(box);   
-        }
-  } else (difficultInput.value == 'Hard');{
-    for (let i = 1; i <= 100; i++) {
-        let box = document.createElement('div');
-        box.classList.add('quadrato3');
-        box.innerHTML = i;
-        containerGame.append(box);   
-        }
-  }
 
 
 }
