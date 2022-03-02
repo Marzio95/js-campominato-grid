c042a2f6201838f47ac50b8231f0fa5b9471fec7
Marzio95 <marziodellarocca14@gmail.com>
2022-03-02 16:52:44 +0100

    completo layout con ancora un importante TODO

--- js/script.js
+++ js/script.js
@@ -1,11 +1,45 @@
 
 const containerGame = document.querySelector('.container_game');
 
-for (let i = 1; i <= 49; i++) {
+// for (let i = 1; i <= 49; i++) {
 
-   let box = document.createElement('div');
-   console.log(box);
-   box.classList.add('quadrato');
-   box.innerHTML = i;
-   containerGame.append(box);   
+//    let box = document.createElement('div');
+//    console.log(box);
+//    box.classList.add('quadrato');
+//    box.innerHTML = i;
+//    containerGame.append(box);   
+// }
+
+const btnPlay = document.getElementById('button_play');
+let difficultInput = document.getElementById('difficult');
+
+btnPlay.addEventListener('click', myGame);
+
+function myGame (){
+    if(difficultInput.value == 'Easy') {
+        
+    for (let i = 1; i <= 49; i++) {
+    let box = document.createElement('div');
+    box.classList.add('quadrato');
+    box.innerHTML = i;
+    containerGame.append(box);   
+    }
+  } else if (difficultInput.value == 'Medium'){
+      
+    for (let i = 1; i <= 81; i++) {
+        let box = document.createElement('div');
+        box.classList.add('quadrato2');
+        box.innerHTML = i;
+        containerGame.append(box);   
+        }
+  } else (difficultInput.value == 'Hard');{
+    for (let i = 1; i <= 100; i++) {
+        let box = document.createElement('div');
+        box.classList.add('quadrato3');
+        box.innerHTML = i;
+        containerGame.append(box);   
+        }
+  }
+
+
 }
